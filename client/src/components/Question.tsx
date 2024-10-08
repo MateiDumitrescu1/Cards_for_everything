3546b36ec8594abf7c2a2d289156b37388f6c787
matei <matei.dumitrescu@gmail.com>
2024-10-09 00:45:30 +0200

    added code editor

--- client/src/components/Question.tsx
+++ client/src/components/Question.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { useRef } from 'react';
 import styles from "../styles/Question.module.scss"
+
+import Editor from '@monaco-editor/react';
 const Question = () => {
+    const editorRef = useRef<any>(null);
+
+    function handleEditorDidMount(editor: any, monaco: any) {
+        editorRef.current = editor;
+    }
+
+    function showValue() {
+        if (editorRef.current) {
+            alert(editorRef.current.getValue());
+        }
+    }
     return (
-        <div className={styles.container}>Question</div>
+        <>
+            <div className={styles.container}>Question</div>
+            <Editor
+                height="90vh"
+                defaultLanguage="javascript"
+                defaultValue="// some comment"
+                onMount={handleEditorDidMount}
+            />
+        </>
     )
 }
 
